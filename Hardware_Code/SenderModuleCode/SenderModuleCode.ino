c847174fbe535efc20f442103736b1f8c91408ee
sarthaktanwani <stanwani7@gmail.com>
2018-10-04 13:24:13 +0530

    Uploading more code in GitHub

---
+++ Hardware_Code/SenderModuleCode/SenderModuleCode.ino
@@ -0,0 +1,188 @@
+/*
+Liquid flow rate sensor -DIYhacking.com Arvind Sanjeev
+
+Measure the liquid/water flow rate using this code. 
+Connect Vcc and Gnd of sensor to arduino, and the 
+signal line to arduino digital pin 2.
+ 
+ */
+#include "TinyGPS++.h"
+#include "SoftwareSerial.h"
+SoftwareSerial serial_connection(3, 5); 
+TinyGPSPlus gps;
+SoftwareSerial GPRS(10, 11);  //RX=pin 10, TX=pin 11
+byte statusLed    = 13;
+
+byte sensorInterrupt = 0;  // 0 = digital pin 2
+byte sensorPin       = 2;
+
+// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
+// litre/minute of flow.
+float calibrationFactor = 4.5;
+
+volatile byte pulseCount;  
+
+float flowRate;
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres;
+unsigned long temp;
+uint8_t hr_val, min_val, sec_val;
+
+String flon,flat,fspeed;
+
+uint8_t flag = 0;
+String message;
+String destinationNumber = "+918447316525";
+
+float lat;
+float lng;
+int speed1;
+
+unsigned long oldTime;
+
+void setup()
+{
+  
+  // Initialize a serial connection for reporting values to the host
+  Serial.begin(9600);
+  GPRS.begin(9600);
+  serial_connection.begin(9600);//This opens up communications to the GPS
+  // Set up the status LED line as an output
+  pinMode(statusLed, OUTPUT);
+  digitalWrite(statusLed, HIGH);  // We have an active-low LED attached
+  
+  pinMode(sensorPin, INPUT);
+  digitalWrite(sensorPin, HIGH);
+
+  pulseCount        = 0;
+  flowRate          = 0.0;
+  flowMilliLitres   = 0;
+  totalMilliLitres  = 0;
+  oldTime           = 0;
+
+  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
+  // Configured to trigger on a FALLING state change (transition from HIGH
+  // state to LOW state)
+  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+}
+
+/**
+ * Main program loop
+ */
+void loop()
+{
+   GetLocation();
+   if((millis() - oldTime) > 1000)    // Only process counters once per second
+  { 
+    // Disable the interrupt while calculating flow rate and sending the value to
+    // the host
+    detachInterrupt(sensorInterrupt);
+        
+    // Because this loop may not complete in exactly 1 second intervals we calculate
+    // the number of milliseconds that have passed since the last execution and use
+    // that to scale the output. We also apply the calibrationFactor to scale the output
+    // based on the number of pulses per second per units of measure (litres/minute in
+    // this case) coming from the sensor.
+    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+    
+    // Note the time this processing pass was executed. Note that because we've
+    // disabled interrupts the millis() function won't actually be incrementing right
+    // at this point, but it will still return the value it was set to just before
+    // interrupts went away.
+    oldTime = millis();
+    // Divide the flow rate in litres/minute by 60 to determine how many litres have
+    // passed through the sensor in this 1 second interval, then multiply by 1000 to
+    // convert to millilitres.
+    flowMilliLitres = (flowRate / 60) * 1000;
+    temp = totalMilliLitres;
+    // Add the millilitres passed in this second to the cumulative total
+    totalMilliLitres += flowMilliLitres;
+    if (temp == totalMilliLitres && totalMilliLitres >= 300)
+    {
+      flag++;
+    }
+    if(flag == 10)
+    {
+      //if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
+    //{
+      lat = gps.location.lat();
+      flat = String(lat, 6);
+      Serial.println(lat, 6);
+      lng = gps.location.lng();
+      flon = String(lng, 6);
+      Serial.println(lng, 6);
+      speed1 = gps.speed.mph();
+      fspeed = String(speed1, 6);
+      Serial.println(fspeed);
+      hr_val = gps.time.hour(); /* Get hour */
+      min_val = gps.time.minute();  /* Get minutes */
+      sec_val = gps.time.second();  /* Get seconds */
+      hr_val = (hr_val + 5) % 12;
+      min_val = (min_val + 30) % 60;
+      String timeString = String(hr_val) + ":" + String(min_val) + ":" + String(sec_val);
+    //}
+      message = "(Quantity=" + (String)totalMilliLitres + ", " + "Latitude=" + flat + ", " + "Longitude=" + flon + ", " + "Speed=" + fspeed + ", " + "Time=" + timeString + ")";
+      SendMessage(destinationNumber, message);
+      flag = 0;
+      totalMilliLitres = 0;
+    }
+    unsigned int frac;
+    
+    // Print the flow rate for this second in litres / minute
+    Serial.print("Flow rate: ");
+    Serial.print(int(flowRate));  // Print the integer part of the variable
+    Serial.print(".");             // Print the decimal point
+    // Determine the fractional part. The 10 multiplier gives us 1 decimal place.
+    frac = (flowRate - int(flowRate)) * 10;
+    Serial.print(frac, DEC) ;      // Print the fractional part of the variable
+    Serial.print("L/min");
+    // Print the number of litres flowed in this second
+    Serial.print("  Current Liquid Flowing: ");             // Output separator
+    Serial.print(flowMilliLitres);
+    Serial.print("mL/Sec");
+
+    // Print the cumulative total of litres flowed since starting
+    Serial.print("  Output Liquid Quantity: ");             // Output separator
+    Serial.print(totalMilliLitres);
+    Serial.println("mL"); 
+
+    // Reset the pulse counter so we can start incrementing again
+    pulseCount = 0;
+    
+    // Enable the interrupt again now that we've finished sending output
+    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+  }
+}
+
+/*
+Insterrupt Service Routine
+ */
+void pulseCounter()
+{
+  // Increment the pulse counter
+  pulseCount++;
+}
+
+void SendMessage(String Number, String Message)
+{
+  Serial.println("Sending message! ...... From Sender Module");
+  GPRS.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+  delay(1000);  // Delay of 1000 milli seconds or 1 second
+  GPRS.println("AT+CMGS=\"" + Number + "\"\r"); // Replace x with mobile number
+  //GPRS.println("AT+CMGS=\"+919910004136\"\r");
+  delay(1000);
+  GPRS.println(Message);// The SMS text you want to send
+  delay(1000);
+  GPRS.println((char)26);// ASCII code of CTRL+Z
+  delay(1000);
+  Serial.println("Message Sent!");
+}
+
+void GetLocation()
+{
+  while(serial_connection.available())//While there are characters to come from the GPS
+  {//Serial.println("Check");
+    gps.encode(serial_connection.read());//This feeds the serial NMEA data into the library one char at a time
+  }
+}
+
---
+++ Hardware_Code/sketch_aug14a/sketch_aug14a.ino
@@ -0,0 +1,95 @@
+/*
+    HTTP over TLS (HTTPS) example sketch
+    This example demonstrates how to use
+    WiFiClientSecure class to access HTTPS API.
+    We fetch and display the status of
+    esp8266/Arduino project continuous integration
+    build.
+    Limitations:
+      only RSA certificates
+      no support of Perfect Forward Secrecy (PFS)
+      TLSv1.2 is supported since version 2.4.0-rc1
+    Created by Ivan Grokhotkov, 2015.
+    This example is in public domain.
+*/
+
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+ 
+const char* ssid = "Shanu1";
+const char* password = "20167635";
+
+const char* host = "urfuel.000webhostapp.com";
+const int httpsPort = 443;
+
+// Use web browser to view and copy
+// SHA1 fingerprint of the certificate
+const char* fingerprint = "42 E9 F5 F9 30 21 14 CD 75 A1 41 EF 39 33 E8 D4 C7 97 B9 7C";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Use WiFiClientSecure class to create TLS connection
+  WiFiClientSecure client;
+  Serial.print("connecting to ");
+  Serial.println(host);
+  while(!client.connect(host, httpsPort))
+  {delay(10);
+    }
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  if (client.verify(fingerprint, host)) {
+    Serial.println("certificate matches");
+  } else {
+    Serial.println("certificate doesn't match");
+  }
+
+  String url = "/insertintable.php?value=3841.00&&user_id=7&&utime=77.284157";
+  Serial.print("requesting URL: ");
+  Serial.println(url);
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "User-Agent: BuildFailureDetectorESP8266\r\n" +
+               "Connection: close\r\n\r\n");
+
+  Serial.println("request sent");
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("headers received");
+      break;
+    }
+  }
+  String line = client.readStringUntil('m');
+  if (line.startsWith("***")) {
+    Serial.println("esp8266/Arduino CI successfull!");
+  } else {
+    Serial.println("esp8266/Arduino CI has failed");
+  }
+  Serial.println("reply was:");
+  Serial.println("==========");
+  Serial.println(line);
+  Serial.println("==========");
+  Serial.println("closing connection");
+}
+
+void loop() {
+}
